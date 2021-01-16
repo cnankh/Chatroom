7bf08dd54b9def86daea4b18a2be63c28de7358a
Sina <36819672+cnankh@users.noreply.github.com>
2021-01-16 17:53:41 +0330

    MVVM arch init
    setup views and models with mvvm arch

---
+++ app/src/main/java/com/example/chatroom/model/User.kt
@@ -0,0 +1,4 @@
+package com.example.chatroom.model
+
+class User {
+}
--- app/src/main/java/com/example/chatroom/view/MainActivity.kt
+++ app/src/main/java/com/example/chatroom/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.chatroom
+package com.example.chatroom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatroom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
